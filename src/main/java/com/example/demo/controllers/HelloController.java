9aedafd651d97cff8ea82ab86eaaa52298325e35
arinamedvedeva-pi202 <94178125+arinamedvedeva-pi202@users.noreply.github.com>
2023-11-15 13:49:30 +0600

    first lab

---
+++ src/main/java/com/example/demo/controllers/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    
+    @GetMapping("/hello")
+    @ResponseBody
+    public ResponseEntity<String> getName (@RequestParam("name") final String firstname ){
+        String response = "hello " + firstname;
+        return ResponseEntity.ok().body(response);
+    }
+}
